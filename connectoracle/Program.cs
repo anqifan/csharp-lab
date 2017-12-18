f8503226ffa6dac93c78e9152a01e6fcdd92f0ae
anqifan <359287221@qq.com>
2017-12-18 10:41:57 +0800

    commit

---
+++ connectoracle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+
+
+namespace connectoracle
+{
+    class Program
+    {
+       
+        public static string ConnectOracle()
+    {
+        try
+        {
+            string connString = "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=192.168.137.250)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=orcl)));Persist Security Info=True;User ID=hr;Password=hr;";
+            OracleConnection con = new OracleConnection(connString);
+
+            con.Open();
+            Console.WriteLine("yes");
+            return string.Empty;
+        }
+        catch (Exception ex)
+        {
+                Console.WriteLine("no");
+                return ex.ToString();
+        }
+    }
+    static void Main(string[] args)
+        {
+            Console.WriteLine(ConnectOracle());
+            Console.ReadLine();
+        }
+    }
+}
